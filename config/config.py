6889927bb919cb10b73fe2b0b297ec018772175d
mirko <mirceadino97@gmail.com>
2018-04-28 17:09:59 +0300

    Update .gititnore, pip requirements and config parameters

--- config/config.py
+++ config/config.py
@@ -1,10 +1,10 @@
 # Model
 NUM_CLASSES = 2
 IMAGE_SIZE = 128
-PATH_MODEL = ""
+PATH_MODEL = "model/model.tflearn"
 
 # Training
-NUM_EPOCHES = 10
+NUM_EPOCHS = 10
 BATCH_SIZE = 128
 SHUFFLE = True
 SNAPSHOT_EPOCH = True
@@ -12,14 +12,15 @@ SNAPSHOT_STEP = 100
 SHOW_METRIC = True
 
 # Classifier
-GENRES = []
+GENRES = sorted(["electro", "classical"])
 
 # Dataset
-PATH_TRAINING_DATASET = ""
-PATH_VALIDATION_DATASET = ""
-PATH_TESTING_DATASET = ""
+PATH_TRAINING_DATASET = "dataset/training"
+PATH_VALIDATION_DATASET = "dataset/validation"
+PATH_TESTING_DATASET = "dataset/testing"
 RATIO_VALIDATION = 0.3
 RATIO_TESTING = 0.1
 
 # Songs
-PATH_SONGS = ""
+PATH_SONGS = "raw_dataset/"
+PATH_SONG_INFO = "raw_dataset/info.csv"
