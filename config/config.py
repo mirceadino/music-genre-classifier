10d11b49084d5f14ac5de3a66fff3b4c1a5d9b39
Ubuntu <mirceadino97@gmail.com>
2018-06-10 18:51:23 +0000

    Modify config params

--- config/config.py
+++ config/config.py
@@ -1,26 +1,28 @@
 # Classifier
-GENRES = sorted(["electro", "classical", "latino"])
+#GENRES = sorted(["electro", "classical", "latino"])
+#GENRES = sorted(["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"])
+GENRES = sorted(["classical", "disco", "hiphop", "jazz", "rock"])
 
 # Model
 NUM_CLASSES = len(GENRES)
 SLICE_SIZE = 128
 SLICE_OVERLAP = 64
-PATH_MODEL = "model/model-04.tflearn"
+PATH_MODEL = "model/model-cnn-02.tflearn"
 
 # Training
 BATCH_SIZE = 128
-SHUFFLE = False
+SHUFFLE = True 
 SNAPSHOT_EPOCH = True
 SNAPSHOT_STEP = None
 SHOW_METRIC = True
 
 # Dataset
-PATH_TRAINING_DATASET = "dataset/training-03"
-PATH_VALIDATION_DATASET = "dataset/validation-03"
-PATH_TESTING_DATASET = "dataset/testing-03"
+PATH_TRAINING_DATASET = "dataset/training-genres"
+PATH_VALIDATION_DATASET = "dataset/validation-genres"
+PATH_TESTING_DATASET = "dataset/testing-genres"
 RATIO_VALIDATION = 0.27
 RATIO_TESTING = 0.1
 
 # Songs
-PATH_SONGS = "raw_dataset/"
-PATH_SONG_INFO = "raw_dataset/info_tiny.csv"
+PATH_SONGS = "raw_dataset-genres/"
+PATH_SONG_INFO = "raw_dataset-genres/info.csv"
