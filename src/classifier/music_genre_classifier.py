a4c9d065facd7c81fc4ab582e963a02d00b0a393
mirko <mirceadino97@gmail.com>
2018-06-19 16:07:30 +0300

    Batch testing fix

--- src/classifier/music_genre_classifier.py
+++ src/classifier/music_genre_classifier.py
@@ -46,10 +46,10 @@ class MusicGenreClassifier:
         predictions = self.__nn.predict_label(slices)
         return self.count_predictions(predictions)
 
-    def test(self, slices, labels, batch_size=128):
+    def test(self, slices, labels, batch=128):
         all_predictions = []
-        for i in range(len(slices), batch_size):
-            j = min(len(slices), i + batch_size)
+        for i in range(0, len(slices), batch):
+            j = min(len(slices), i + batch)
             all_predictions.extend(self.__nn.predict_label(slices[i:j]))
         slices_per_song = len(slices) // len(labels)
         matches = 0
