a4e2d6a66dbbeeeb3f327d5aa3cfd2986d5ad59b
Ubuntu <mirceadino97@gmail.com>
2018-06-22 18:44:36 +0000

    Config file

--- config/config.py
+++ config/config.py
@@ -1,13 +1,12 @@
 # Classifier
 GENRES = sorted(["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"])
-GENRES = sorted(["latino", "electro", "classical"])
 
 # Model
 NUM_CLASSES = len(GENRES)
 SLICE_HEIGHT = 128
 SLICE_WIDTH = 128
 SLICE_OVERLAP = 64
-PATH_MODEL = "resources/model/gtzan-10-model-yy.tflearn"
+PATH_MODEL = "resources/model/gtzan-10/model.tflearn"
 
 # Training
 BATCH_SIZE = 128
@@ -17,12 +16,12 @@ SNAPSHOT_STEP = None
 SHOW_METRIC = True
 
 # Dataset
-PATH_TRAINING_DATASET = "resources/dataset/gtzan-10-training-yy"
-PATH_VALIDATION_DATASET = "resources/dataset/gtzan-10-validation-yy"
-PATH_TESTING_DATASET = "resources/dataset/gtzan-10-testing-yy"
-RATIO_VALIDATION = 0.27
+PATH_TRAINING_DATASET = "resources/dataset/gtzan-10/training"
+PATH_VALIDATION_DATASET = "resources/dataset/gtzan-10/validation"
+PATH_TESTING_DATASET = "resources/dataset/gtzan-10/testing"
+RATIO_VALIDATION = 0.10
 RATIO_TESTING = 0.10
 
 # Songs
-PATH_SONGS = "resources/raw_dataset/"
-PATH_SONG_INFO = "resources/raw_dataset/info.csv"
+PATH_SONGS = "resources/raw_dataset/gtzan-10/"
+PATH_SONG_INFO = "resources/raw_dataset/gtzan-10/info.csv"
