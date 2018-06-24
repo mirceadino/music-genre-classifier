97285324e6bc769652a7d36d33ba115370972d15
mirko <mirceadino97@gmail.com>
2018-06-24 20:25:13 +0300

    Config file

--- config/config.py
+++ config/config.py
@@ -1,11 +1,11 @@
 # General
-NAME = "gtzan-10"
+NAME = "small-yt"
 ROOT_MODEL = "resources/model/" + NAME + "/"
 ROOT_DATASET = "resources/dataset/" + NAME + "/"
 ROOT_SONGS = "resources/raw_dataset/" + NAME + "/"
 
 # Classifier
-GENRES = sorted(["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"])
+GENRES = sorted(["electro", "latino"])
 
 # Model
 NUM_CLASSES = len(GENRES)
@@ -25,10 +25,9 @@ SHOW_METRIC = True
 PATH_TRAINING_DATASET = ROOT_DATASET + "training"
 PATH_VALIDATION_DATASET = ROOT_DATASET + "validation"
 PATH_TESTING_DATASET = ROOT_DATASET + "testing"
-RATIO_VALIDATION = 0.10
+RATIO_VALIDATION = 0.20
 RATIO_TESTING = 0.10
 
 # Songs
 PATH_SONGS = ROOT_SONGS
 PATH_SONG_INFO = ROOT_SONGS + "info.csv"
-
